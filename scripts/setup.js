a453929d05dc85c3d4cc82816a35fb92fbb9ef43
Roshini <roshni.dabriya@gmail.com>
2025-06-30 17:22:22 +0530

    Add setup logic

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,1 +1,22 @@
-window.name = "Roro";
+let joinButton = document.getElementById("join");
+joinButton.addEventListener("click", joinRoom);
+
+let hostButton = document.getElementById("host");
+hostButton.addEventListener("click", hostRoom);
+
+let nameInp = document.getElementById("name");
+function joinRoom() {
+    if (nameInp.value.trim() != "") {
+        window.name = nameInp.value.trim();
+        window.location.href = "/chat.html";
+    }
+    else {
+        alert("Enter your name first!");
+    }
+}
+
+function hostRoom() {
+    if (nameInp.value.trim() != "") {
+
+    }
+}
