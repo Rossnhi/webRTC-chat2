19b8687ad303d32e4cad2cd9ec6f8efeed62550b
Roshini S <roshni.dabriya@gmail.com>
2025-12-13 14:10:09 +0530

    Generate room code for hosting

--- scripts/setupIndex.js
+++ scripts/setupIndex.js
@@ -5,10 +5,10 @@ let hostButton = document.getElementById("host");
 hostButton.addEventListener("click", hostRoom);
 
 let nameInp = document.getElementById("name");
+let codeInp = document.getElementById("roomCode");
 function joinRoom() {
-    if (nameInp.value.trim() != "") {
-        window.name = nameInp.value.trim();
-        window.location.href = `chat.html?name=${encodeURIComponent(window.name.trim())}`;
+    if (nameInp.value.trim() != "" && codeInp.value.trim() != "") {
+        window.location.href = `chat.html?name=${encodeURIComponent(nameInp.value.trim())}&room=${encodeURIComponent(codeInp.value.trim())}&hosting=false`;
     }
     else {
         alert("Enter your name and room code first!");
@@ -17,7 +17,18 @@ function joinRoom() {
 
 function hostRoom() {
     if (nameInp.value.trim() != "") {
-
+        window.location.href = `chat.html?name=${encodeURIComponent(nameInp.value.trim())}&room=${encodeURIComponent(generateRoomCode())}&hosting=true`;
+    }
+    else {
+        alert("Enter your name first!");
     }
 }
 
+// Generate random 4 letter room code
+function generateRoomCode() {
+    let code = "";
+    for (let i = 0; i < 4; i++) {
+        code += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    return code;
+}
